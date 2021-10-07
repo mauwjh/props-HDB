7d30049dbfda61bc9569931fca885651605e0ec7
mauwjh <matthew.auw@gmail.com>
2021-10-07 14:11:56 +0800

    secured API key

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -5,10 +5,9 @@ import '../Map.css'
 const Map = ({ location, zoomLevel }) => (
   <div className="map">
     <h2 className="map-h2">Come Visit Us At Our Campus</h2>
-
     <div className="google-map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCoRQxxeFYtKnmI2DsjiSklGXl_gtR6dCk' }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
         defaultCenter={location}
         defaultZoom={zoomLevel}
       >
