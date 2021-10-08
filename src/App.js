5d8e59f76b66d41ee94c5bd8e9e4857d7f1903ef
mauwjh <matthew.auw@gmail.com>
2021-10-08 10:57:30 +0800

    abcd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import Map from "./Components/Map";
 import EnhancedTable from "./Components/DataTable";
 
 function App() {
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
-  const url =
-    "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3";
+  // const url =
+  //   "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3";
 
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      })
-      .catch(() => console.log("error"));
-  }, []);
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       console.log(data);
+  //     })
+  //     .catch(() => console.log("error"));
+  // }, []);
 
   const location = {
     address: "374 Laurel Wood Avenue",
